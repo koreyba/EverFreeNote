54d66865ad435726dc50304c3bfaa05749c592b3
Denys Koreiba <denys_koreiba@aprimo.com>
2025-11-30 15:42:43 +0100

    удалил старые файлы

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,28 +2,6 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   projectId: '76trp2',
-  e2e: {
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js',
-    setupNodeEvents() {
-      // implement node event listeners here
-    },
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    video: true,
-    screenshotOnRunFailure: true,
-    experimentalRunAllSpecs: true,
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 30000,
-    requestTimeout: 10000,
-    responseTimeout: 30000,
-    experimentalPromptCommand: true,
-    retries: {
-      runMode: 0,
-      openMode: 0,
-    },
-  },
   component: {
     devServer: {
       framework: 'next',
