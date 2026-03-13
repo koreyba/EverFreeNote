16835e30e341714459bfd38e780bbf333e13a610
Denys <koreybadenis@gmail.com>
2026-03-13 21:26:48 +0100

    Add SettingsPage component

---
+++ app/settings/page.tsx
@@ -0,0 +1,9 @@
+export default function SettingsPage() {
+  const message = "\u0440\u043e\u0443\u0442 \u0440\u0430\u0431\u043e\u0442\u0430\u0442\u0435"
+
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <p>{message}</p>
+    </main>
+  )
+}
